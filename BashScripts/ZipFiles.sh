238a90c0cc616a558acf3773dc6fd132d575dee5
FlynnEastwood <af93@protonmail.com>
2021-05-29 15:03:53 -0400

    Added ZipFiles
    Can zip multiple files and directories to an archive.

---
+++ BashScripts/ZipFiles.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+# -r est pour inclure les subdirectories récursivement
+# Nom de l'archive
+# Ajout avec espace, des noms des fichiers et dossiers
+zip -r SM_3DModels.zip Blender Formats Textures README.pdf
+
