93815dbb7ac19cdd8e2d210db88ed493ad14c658
FlynnEastwood <af93@protonmail.com>
2021-05-29 00:44:11 -0400

    Added renaming function
    Will now change the name correctly for the new png

--- BashScripts/imgConverter.sh
+++ BashScripts/imgConverter.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+mkdir png
+
 for i in ./*.tga ./*.TGA
 
 do	
-	magick convert -flip $i $i.png
-	sed 's/S//'	
+	magick convert -flip $i ./png/$i.png;
+done
+
+cd png
+
+for i in ./*.png
+	do newfile=${i/.tga.png/.png}; 
+	mv $i $newfile;
 done
