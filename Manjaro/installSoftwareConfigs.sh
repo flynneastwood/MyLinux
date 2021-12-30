033901332b6b0b38d5c36444ac926ffdebfcec02
flynneastwood <af93@protonmail.com>
2021-12-29 19:26:52 -0500

    tried fixing config blender but not really working yet

--- Manjaro/installSoftwareConfigs.sh
+++ Manjaro/installSoftwareConfigs.sh
@@ -2,19 +2,19 @@
 
  function config_blender {
 	echo "Download Blender configs from Github" 
-	git clone https://github.com/flynneastwood/blenderconfigs
+	git clone https://github.com/flynneastwood/blenderconfigs /home/$USER/Downloads
 
 	#Installing base scene template        
-	mkdir -p /home/$USER/.config/blender/2.93/config/	
-	cp ./blenderconfigs/startup.blend ./blenderconfigs/userpref.blend /home/$USER/.config/blender/2.93/config
+	mkdir -p /home/$USER/.config/blender/3.0/config/	
+	cp /home/$USER/Downloads/startup.blend /home/$USER/Downloads/userpref.blend /home/$USER/.config/blender/3.0/config
 	echo "Base startup scene installed" 	
 	
 	#Video editing templates install	
-	cp ./blenderconfigs/Video_Editing/startup.blend /usr/bin/blender-*/2.*/scripts/startup/bl_app_templates_system/Video_Editing
+	cp /home/$USER/Downloads/Video_Editing/startup.blend /usr/share/blender/3.*/scripts/startup/bl_app_templates_system/Video_Editing
 	echo "Video editing template installed"
 	
 	#VFX templates install	
-	cp ./blenderconfigs/VFX/startup.blend /usr/bin/blender-*/2.*/scripts/startup/bl_app_templates_system/VFX
+	cp /home/$USER/Downloads/VFX/startup.blend /usr/share/blender/3.*/scripts/startup/bl_app_templates_system/VFX
 	echo "VFX template installed" 
 }
 
