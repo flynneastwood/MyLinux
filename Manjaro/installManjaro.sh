d226dd46be1b415e88e84fcb67ffa074c5525cce
FlynnEastwood <af93@protonmail.com>
2023-04-12 00:52:07 -0400

    Auto install softwares work

--- Manjaro/installManjaro.sh
+++ Manjaro/installManjaro.sh
@@ -29,42 +29,12 @@ sudo pacman -Syu --noconfirm #No confrim command does not work.
 
 echo "Installing softwares."
 
-#Installs a barebone calculator
-pamac install pantheon-calculator --no-confirm #Does not finish?
-
-echo "Installs Blender"
-pamac install blender --no-confirm
-
-echo "Installs Krita"
-sudo pamac install krita --no-confirm
-
-echo "Installs OBS"
-sudo pamac install obs-studio --no-confirm
-
-echo "Installs JACK"
-sudo pamac install qjackctl --no-confirm
-
-echo "Installs the image viewer"
-sudo pamac install gpicview --no-confirm
-
-
-#Too long to install. Synchronizing packages infinitely?
-echo "Installs LibreOffice"
-sudo pamac install libreoffice-fresh --no-confirm
+sudo pacman -S --noconfirm --needed blender krita obs-studio gpicview libreoffice-still baobab pantheon-calculator reaper discord
 
 echo "Installs Brave browser"
 sudo pacman-mirrors --api --set-branch stable && echo "Server = https://brave-browser-arch.stable.arhag.io/\$arch/" | sudo tee -a /etc/pacman.conf && sudo pacman-key --recv-key 3050AC3CD2AE6F03 && sudo pacman-key --lsign-key 3050AC3CD2AE6F03
 sudo pacman -Syy
 sudo pacman -S brave-browser
-
-echo "Installs disk analyser"
-sudo pamac install baobab --no-confirm
-
-echo "Installs Reaper"
-sudo pamac install reaper-bin --no-confirm
-
-echo "Installs Discord"
-sudo pamac install discord --no-confirm
 
 echo "Done installing softwares."
 
