726a665b4e48e56bbf5f74c8c568b08227650252
FlynnEastwood <af93@protonmail.com>
2023-05-15 14:02:18 -0400

    Update ConfigureBlender.sh
    Add the blender file to .config folder

--- Manjaro/ConfigureBlender.sh
+++ Manjaro/ConfigureBlender.sh
@@ -14,6 +14,7 @@ CUSTOM_STARTUP="$HOME/BlenderConfigs/startup.blend"
 BLENDER_VERSION=$(blender --version | awk 'NR==1 {print $2}' | awk '{print substr($0,1,3)}')
 
 # Step 5: Create version specific config folder
+mkdir $HOME/.config/blender
 CONFIG_FOLDER="$HOME/.config/blender/$BLENDER_VERSION"
 mkdir -p "$CONFIG_FOLDER"
 
